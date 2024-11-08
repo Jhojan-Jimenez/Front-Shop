82f41f75dcc724ff88976062ef7e84bf971880e6
Jhojan Jimenez <jhojanjimene@gmail.com>
2024-11-08 17:22:48 -0500

    Lint fixes 2

--- src/app/lib/actions/category.ts
+++ src/app/lib/actions/category.ts
@@ -1,11 +1,13 @@
 'use server';
 import axios from 'axios';
 import 'server-only';
-const BACK_API = process.env.MORTSHOP_API;
+const NEXT_PUBLIC_MORTSHOP_API = process.env.MORTSHOP_API;
 const api = axios.create({
-	baseURL: `${BACK_API}api/category/`,
+	baseURL: `${NEXT_PUBLIC_MORTSHOP_API}api/category/`,
 });
 export async function getCategories() {
+	console.log(NEXT_PUBLIC_MORTSHOP_API);
+	
 	const res = await api.get('categories');
 	return res.data.categories;
 }
