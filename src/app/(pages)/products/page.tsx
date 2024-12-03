8582a5edf8efc08f7009db24d81cb4b6bb645ad3
Jhojan Jimenez <jhojanjimene@gmail.com>
2024-12-03 13:55:05 -0500

    Filter products 3

--- src/app/(pages)/products/page.tsx
+++ src/app/(pages)/products/page.tsx
@@ -1,32 +1,38 @@
 'use client';
 
-import { Suspense, useEffect, useState } from 'react';
+import { Suspense, useEffect, useState, useMemo } from 'react';
+import { useSearchParams } from 'next/navigation';
 import { getCategories } from '@/app/lib/actions/category';
 import { getProducts } from '@/app/lib/actions/product';
-import { CategorySchema, FilterOptions, ProductSchema } from '@/app/lib/types';
+import { CategorySchema, ProductSchema } from '@/app/lib/types';
 import Index from '@/app/ui/ProductsCatalog/Filters/Index';
 import ProductCatalog from '@/app/ui/ProductsCatalog/ProductCatalog';
 import ProductList from '@/app/ui/ProductsCatalog/ProductList';
 import BeatLoader from 'react-spinners/BeatLoader';
 export const dynamic = 'force-dynamic';
 
-export default function Page({
-	searchParams,
-}: {
-	searchParams: FilterOptions;
-}) {
+export default function Page() {
 	const [categories, setCategories] = useState<[CategorySchema] | []>([]);
 	const [products, setProducts] = useState<[ProductSchema] | []>([]);
 	const [loading, setLoading] = useState(true);
+
+	// Obtener los parámetros de búsqueda desde la URL
+	const searchParams = useSearchParams();
+
+	// Memorizar filters para evitar referencias nuevas
+	const filters = useMemo(
+		() => Object.fromEntries(searchParams.entries()),
+		[searchParams]
+	);
 
 	useEffect(() => {
 		const fetchData = async () => {
 			setLoading(true);
 			try {
 				const categoriesData = await getCategories();
-				console.log('SearchParams:', searchParams);
+				console.log('Filters:', filters);
 
-				const productsData = await getProducts({ ...searchParams });
+				const productsData = await getProducts(filters);
 				console.log(productsData);
 
 				setCategories(categoriesData);
@@ -39,7 +45,7 @@ export default function Page({
 		};
 
 		fetchData();
-	}, [searchParams]);
+	}, [filters]); // Ahora el efecto solo se ejecutará si los valores en `filters` cambian
 
 	return (
 		<section className='bg-gray-50 py-8 antialiased md:py-12'>
@@ -54,8 +60,7 @@ export default function Page({
 					<ProductCatalog categories={categories}>
 						{loading ? (
 							<div className='flex justify-center h-full items-center'>
-								{' '}
-								<BeatLoader />{' '}
+								<BeatLoader />
 							</div>
 						) : (
 							<div className='mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4'>
@@ -63,7 +68,9 @@ export default function Page({
 							</div>
 						)}
 						<div className='w-full text-center'>
-							<Index totalItems={products.length} />
+							<Index
+								totalItems={Array.isArray(products) ? products.length : 0}
+							/>
 						</div>
 					</ProductCatalog>
 				</Suspense>
