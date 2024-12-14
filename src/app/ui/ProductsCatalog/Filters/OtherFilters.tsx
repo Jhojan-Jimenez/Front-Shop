d9a23a71a2ffb7ac8d4f30718856f054b4fefd04
Jhojan Jimenez <jhojanjimene@gmail.com>
2024-12-13 20:55:50 -0500

    Eliminate console.log

--- src/app/ui/ProductsCatalog/Filters/OtherFilters.tsx
+++ src/app/ui/ProductsCatalog/Filters/OtherFilters.tsx
@@ -101,8 +101,6 @@ export function FilterComponent({
 	filter: Filter;
 	isMobile?: boolean;
 }) {
-	console.log(filter);
-
 	const containerClass = isMobile
 		? 'border-t border-gray-200 px-4 py-6'
 		: 'border-b border-gray-200 py-6';
